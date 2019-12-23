42a8f210a5cd969a683dba690df9587254452d6a
MendezDylan <dylan97cr@gmail.com>
2019-12-23 14:33:16 -0600

    Primer commit

---
+++ SalasTEC/src/modelo/Sala.java
@@ -0,0 +1,25 @@
+
+package modelo;
+
+import java.util.ArrayList;
+
+/**
+ * @author Dylan Mendez
+ * @author Jeremy Jimenez
+ * @author Fabian Navarro
+ */
+public class Sala {
+  private static int cantidadSalas=0;
+  private int idSala;
+  private String ubicacion;
+  private int capacidadMaxima;
+  private int capacidadMinima;
+  private int calificacion;
+  private int numTelefono;
+  private ArrayList<Recurso> listaRecursos;
+  private ArrayList<Horario> listaHorarios;
+  private ArrayList<HorarioExcepcion> listaExcepciones;
+  
+  
+  
+}
